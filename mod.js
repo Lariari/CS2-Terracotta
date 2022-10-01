ca65351d656e3b3e169df4c104eb4f3e68f5e8af
Laroldstein <43853367+Lariari@users.noreply.github.com>
2022-10-01 12:51:49 -0500

    config
    Uploaded config scripts/files

---
+++ mod.js
@@ -0,0 +1,11 @@
+config.addBlockIdProperty("teraId", 526);
+mod.addBlock("tera.js", "normal");
+
+config.addBlockIdProperty("teraBId", 527);
+mod.addBlock("teraB.js", "normal");
+
+config.addBlockIdProperty("teraCId", 528);
+mod.addBlock("teraC.js", "normal");
+
+config.addBlockIdProperty("teraDId", 529);
+mod.addBlock("teraD.js", "normal");
